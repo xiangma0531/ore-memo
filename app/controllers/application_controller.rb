2b3f8e18c4fed76a3155dc493b07436a39349f8c
xiangma0531 <shoma0531@outlook.com>
2022-04-23 22:14:16 +0900

    memosテーブル作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  befoer_action :devise_parameter_sanitizer, if: :devise_controller?
+  before_action :devise_parameter_sanitizer, if: :devise_controller?
 
   def configure_permitted_parameter
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
