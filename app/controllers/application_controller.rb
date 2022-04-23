835ab1b813fdded2b533b04972c63bb8e077bec4
xiangma0531 <shoma0531@outlook.com>
2022-04-23 22:10:48 +0900

    新規登録・ログイン・その他画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  befoer_action :devise_parameter_sanitizer, if: :devise_controller?
+
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
