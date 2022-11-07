05436552b0b57cba3e2b1bb3ebed7fc963ae9504
xiangma0531 <shoma0531@outlook.com>
2022-11-08 02:48:08 +0900

    トリガー設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(params[:user].permit(:name, :email, :password, :password_confirmation))
+    super
+    if @user.save
+      UserMailer.account_activation(@user).deliver_now
+    end
+  end
 
   # GET /resource/edit
   # def edit
