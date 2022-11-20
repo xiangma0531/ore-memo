c80b0a05429db9e23396bb6074c67f594f90867e
xiangma0531 <shoma0531@outlook.com>
2022-11-20 15:58:57 +0900

    パスワードリセットメール送信設定(開発環境のみ)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,8 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000, protocol: 'http' }
+
   # config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
