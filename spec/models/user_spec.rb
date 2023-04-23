50af9fce98a694fc452d6596b6546950ddc2d692
xiangma0531 <shoma0531@outlook.com>
2023-04-23 13:50:40 +0900

    Userモデル単体テスト実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # インスタンス生成
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  # ユーザー新規登録のテストコード
   describe 'ユーザー新規登録' do
-    # ユーザー新規登録についてのテストコード
 
     it 'nameが空では登録されない' do
-      
+      @user.name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("ユーザー名を入力してください")
     end
 
     it 'emailが空では登録されない' do
-      
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Eメールを入力してください")
     end
   end
 end
