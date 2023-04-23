2e95fc01b84d6654891334be7ff8178d74d77caf
xiangma0531 <shoma0531@outlook.com>
2023-04-23 17:05:19 +0900

    Memoモデル単体テスト実装

--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Memo, type: :model do
   # インスタンス生成
   before do
-    @user = FactoryBot.build(:user)
+    @memo = FactoryBot.build(:memo)
   end
 
   # メモの投稿に関するテストコード
@@ -12,33 +12,50 @@ RSpec.describe Memo, type: :model do
     # 正常系
     context '新規登録できる場合' do
 
-      it 'タイトルとメモの内容が入力されていれば登録できる' do
-        
+      it 'titleとcontentが入力されていれば登録できる' do
+        @memo.image = nil
+        expect(@memo).to be_valid
       end
 
-      it 'タイトルの入力と画像の添付ができていれば登録できる' do
-        
+      it 'titleの入力とimageの添付ができていれば登録できる' do
+        @memo.content = ''
+        expect(@memo).to be_valid
       end
 
-      it 'タイトルとメモの内容の入力と画像の添付ができていれば登録できる' do
-        
+      it 'titleとcontentの入力とimageの添付ができていれば登録できる' do
+        expect(@memo).to be_valid
       end
 
     end
 
     # 異常系
     context '新規登録できない場合' do
+
+      it 'titleとcontentが未入力かつimageが添付されていない場合登録できない' do
+        @memo.title = ''
+        @memo.content = ''
+        @memo.image = nil
+        @memo.valid?
+        expect(@memo.errors.full_messages).to include("タイトルを入力してください", "内容を入力してください")
+      end
       
-      it 'タイトルが空の場合登録できない' do
-        
+      it 'titleが空の場合登録できない' do
+        @memo.title = ''
+        @memo.valid?
+        expect(@memo.errors.full_messages).to include("タイトルを入力してください")
       end
 
-      it 'メモの内容が未入力かつ画像が添付されていない場合登録できない' do
-        
+      it 'contentとimageがともに空の場合登録できない' do
+        @memo.content = ''
+        @memo.image = nil
+        @memo.valid?
+        expect(@memo.errors.full_messages).to include("内容を入力してください")
       end
 
       it 'userが紐づいていないと登録できない' do
-        
+        @memo.user = nil
+        @memo.valid?
+        expect(@memo.errors.full_messages).to include("Userを入力してください")
       end
       
     end
