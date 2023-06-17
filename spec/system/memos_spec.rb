fe4df0ad31dac2570803b0782c4ad174e88e3939
xiangma0531 <shoma0531@outlook.com>
2023-06-17 12:48:58 +0900

    新規メモ投稿結合テスト

--- spec/system/memos_spec.rb
+++ spec/system/memos_spec.rb
@@ -3,31 +3,54 @@ require 'rails_helper'
 RSpec.describe "メモ投稿", type: :system do
   before do
     @user = FactoryBot.create(:user)
+    @image_path = Rails.root.join('public/images/dammy.png')
   end
 
   context 'メモの投稿ができるとき' do
     it 'ログインしたユーザーは新規投稿できる' do
       # ログインする
+      sign_in(@user)
       # 新規投稿ページへのボタンが表示されている
+      expect(page).to have_content('+')
       # 投稿ページに遷移する
+      visit new_memo_path
       # フォームに情報を入力する
+      fill_in 'タイトル', with: 'タイトル'
+      fill_in '内容', with: Faker::Lorem.sentence
+      attach_file('memo[image]', @image_path, make_visible: true)
       # 「作成」ボタンをクリックすると、Memoモデルのカウントが1上がる
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Memo.count }.by(1)
       # ホーム画面に遷移する
+      expect(current_path).to eq(root_path)
       # ホーム画面に投稿したメモが表示されている
+      expect(page).to have_content('タイトル')
     end
   end
 
   context 'メモの投稿ができないとき' do
     it 'ログインしていないと新規投稿ページに遷移できない' do
       # ホーム画面に遷移する
+      visit root_path
       # 新規投稿ページへのボタンが表示されていない
+      expect(page).to have_no_content('+')
     end
 
     it '入力項目が空のとき、メモの投稿に失敗する' do
       # ログインする
+      sign_in(@user)
       # 新規投稿ページへのボタンが表示されている
+      expect(page).to have_content('+')
       # 投稿ページに遷移する
+      visit new_memo_path
       # 何も入力せずに「作成ボタン」をクリックすると、新規投稿画面に戻り、Memoモデルのカウントも増えない
+      fill_in 'タイトル', with: ''
+      fill_in '内容', with: ''
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Memo.count }.by(0)
+      expect(current_path).to eq(memos_path)
     end
   end
 
