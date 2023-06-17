3d40cb7fdfc3b4924e2900cb1aa9b11e7c375011
xiangma0531 <shoma0531@outlook.com>
2023-06-17 12:22:56 +0900

    メモ投稿結合テストexample整理

--- spec/system/memos_spec.rb
+++ spec/system/memos_spec.rb
@@ -1,9 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "Memos", type: :system do
+RSpec.describe "メモ投稿", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @memo = FactoryBot.create(:memo)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'メモの投稿ができるとき' do
+    it 'ログインしたユーザーは新規投稿できる' do
+      # ログインする
+      # 新規投稿ページへのボタンが表示されている
+      # 投稿ページに遷移する
+      # フォームに情報を入力する
+      # 送信するとMemoモデルのカウントが1上がる
+      # ホーム画面に遷移する
+      # ホーム画面に投稿したメモが表示されている
+    end
+  end
+
+  context 'メモの投稿ができないとき' do
+    it 'ログインしていないと新規投稿ページに遷移できない' do
+      # ホーム画面に遷移する
+      # 新規投稿ページへのボタンが表示されていない
+    end
+  end
+
 end
