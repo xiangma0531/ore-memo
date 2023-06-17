d329158e2b1b2ed525265bd0f1be6a870f26d06c
xiangma0531 <shoma0531@outlook.com>
2023-06-17 11:43:34 +0900

    ユーザー編集機能結合テスト

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -4,6 +4,5 @@ module SignInSupport
     fill_in 'Eメール', with: user.email
     fill_in 'パスワード', with: user.password
     find('input[name="commit"]').click
-    expect(current_path).to eq(root_path)
   end
 end
